1fd2038c7e9168fba2850679b1b9b56d8dc5726d
icitified <icitified@gmail.com>
2022-03-15 18:19:44 +0530

    All clear

--- music.js
+++ music.js
@@ -2,10 +2,13 @@ const fs = require('fs')
 const path = require('path')
 const scdl = require('soundcloud-downloader').default
 
-scdl
-  .downloadPlaylist('https://soundcloud.com/1matp/sets/gou?si=7cc7dab296614f9c9bb5b64946a26a3d&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing')
+var urlPlaylist = '',
+    downloadPath = '';
+
+  scdl
+  .downloadPlaylist(urlPlaylist)
   .then(([streams, trackNames]) => {
     streams.forEach((val, idx) => {
-      val.pipe(fs.createWriteStream(path.join('/Users/matangpatel/Desktop/Empty/music/playlists/gou', trackNames[idx] + '.mp3')))
+      val.pipe(fs.createWriteStream(path.join(downloadPath, trackNames[idx] + '.mp3')))
     })
   })
